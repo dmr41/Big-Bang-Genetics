b7c1b933f61c1b30956566327ffc803c54ef4abc
David Rivers <dmr41.rivers@gmail.com>
2015-01-23 20:22:59 -0700

    still not working on upload of 1.2million records. changed chunk size to 10K

--- app/models/disease.rb
+++ app/models/disease.rb
@@ -24,7 +24,7 @@ class Disease < ActiveRecord::Base
   def self.import(file)
     Disease.delete_all
     file = file.path
-    options = {chunk_size: 100, keep_original_headers: true}
+    options = {chunk_size: 10000, keep_original_headers: true}
     log_counter = 0
     SmarterCSV.process(file, options) do |chunk|
       chunk.each do |data_hash|
@@ -32,7 +32,7 @@ class Disease < ActiveRecord::Base
           Disease.create!(data_hash)
         # end
       end
-      log_counter +=100
+      log_counter +=10000
       puts "---------------#{log_counter}--------------------"
     end
   end
