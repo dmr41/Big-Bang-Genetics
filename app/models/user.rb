bb0c4f2d24a6481d79f5a0c26fe0c3c60a71b787
David Rivers <dmr41.rivers@gmail.com>
2015-02-23 08:35:22 -0700

    removed age requirement for signup and added warning message on registration page for new users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 
   validates :email, presence: true, uniqueness: true
-  validates :first_name, :last_name, :age, presence: true
+  validates :first_name, :last_name, presence: true
   has_secure_password
 
 end
