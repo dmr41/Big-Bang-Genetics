1656bef277e0e7d7771f3559ffb7d18309f423d2
David Rivers <dmr41.rivers@gmail.com>
2015-05-29 09:28:25 +0200

    fixed cancer_spec end-of-input error problem

--- spec/features/cancer_spec.rb
+++ spec/features/cancer_spec.rb
@@ -3,18 +3,6 @@ require "rails_helper"
 feature "cancer"do
 
   scenario "COSMIC login in page" do
-  # 
-  #   # WebDriver driver = new FirefoxDriver(new FirefoxBinary(new File("path/to/your/firefox.exe")), profile);
-  #   Capybara.run_server = false
-  #   Capybara.current_driver = :selenium
-  #   Capybara.app_host = 'https://cancer.sanger.ac.uk'
-  #   visit('/cosmic/login')
-  #   find("div > form > dl > dd > input")
-  #   # fill_in "Email", with: "dmrivers@syr.edu"
-  #   # fill_in "Password", with: "cosmorios73#"
-  #   # find('ol li a', text: 'The Big Dig').click
-  #   # click_on "Login>>"
-  # end
-
+  end
 
 end
